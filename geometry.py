3608950135be00eb8348e792ab3f90b255c4bd22
Elijah Escamillagit <elijah.escamilla@gmail.com>
2024-05-22 02:32:59 -0700

    first commit

---
+++ geometry.py
@@ -0,0 +1,14 @@
+def calculate_square_footage(length, width):
+    """
+    Calculate the square footage of a house.
+    Formula: Length x Width
+    """
+    return length * width
+
+def calculate_circumference(radius):
+    """
+    Calculate the circumference of a circle.
+    Formula: 2 * π * radius
+    """
+    import math
+    return 2 * math.pi * radius
